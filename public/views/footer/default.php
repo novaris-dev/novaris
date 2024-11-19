fc058c89b9e74a8e66e175da3dd5c4bc95cc8f29
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-11-19 18:48:23 +0000

    add theme specific in footer

--- public/views/footer/default.php
+++ public/views/footer/default.php
@@ -1,7 +1,7 @@
 <footer id="colophon" class="site-footer">
 	<div class="site-info">
 		<?= Novaris\PoweredBy::render(); ?><br />
-		Powered By <a href="https://github.com/novaris-dev">Novaris</a>
+		Powered By <a href="https://github.com/novaris-dev">Novaris</a> and <a href="https://github.com/novaris-dev/amicable">Amicable</a>
 	</div>
 </footer>
 </div>
